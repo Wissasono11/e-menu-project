38c738e6b6dcf2803de7a9bfde055623e5f604da
Wissasono11 <bayuwicaksono782@gmail.com>
2025-09-06 21:04:15 +0700

    feat:integrated detail product for UI

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\FrontendController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/{username}', [FrontendController::class, 'index'])->name('index');
+Route::get('/{username}/product/{id}', [ProductController::class, 'show'])->name('product.show');
